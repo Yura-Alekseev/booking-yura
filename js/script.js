9cfc83d8b7c83de0c88e2f74bc7ffe7ae74099fe
Yura Alekseev <yuraalekseev00@gmail.com>
2020-04-23 15:56:18 +0300

    added header, banner, burger, navigation components and some fonts

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let header = document.getElementById('header');
+let burgerMenu = header.querySelector('.burger__toggle');
+
+burgerMenu.addEventListener('change', () => {
+    header.classList.toggle('burger--open');
+});
